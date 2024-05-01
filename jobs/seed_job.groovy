d589135228959ba32c8d77923a00c49917582910
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2024-05-01 14:16:11 +0530

    Create seed_job.groovy

---
+++ jobs/seed_job.groovy
@@ -0,0 +1,21 @@
+job("test_folder/test_freestyle_job"){
+    steps {
+        shell('echo Hello World!')
+    }
+}
+
+pipelineJob('test_folder/test_pipeline_job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/lyfofvipin/jenkins_tutorials.git')
+                    }
+                    branches("master")
+                }
+            }
+            scriptPath('jobs/First_Jenkinsfile')
+        }
+    }
+}
